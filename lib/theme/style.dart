5e6680e736e33bfcdb89dd074fe113d08c80ab22
MattViper <mattoshinigami@gmail.com>
2020-03-22 19:25:24 +0100

    Folder structure, init styles

---
+++ lib/theme/style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme() {
+  return ThemeData(
+    primaryColor: Colors.blue,
+    accentColor: Colors.greenAccent,
+    hintColor: Colors.white,
+    dividerColor: Colors.blueGrey,
+    buttonColor: Colors.lightBlue,
+    scaffoldBackgroundColor: Colors.white,
+    canvasColor: Colors.white
+  );
+}
