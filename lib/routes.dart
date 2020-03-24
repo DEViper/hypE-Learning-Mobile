7eee890c337ec19ea241a009d435664f069ec2d0
MattViper <mattoshinigami@gmail.com>
2020-03-24 03:18:26 +0100

    Login Page UI

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
-import 'package:hype_learning_flutter/screens/home/home.dart';
+import 'package:hype_learning_flutter/main.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  "/": (BuildContext context) => Home(),
+  "/": (BuildContext context) => MyApp(),
   
 };
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:hype_learning_flutter/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(HypeLearning());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
