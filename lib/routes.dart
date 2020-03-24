2ee8b0dac1dd17ccbb40483877a6615e4e2b48c3
MattViper <mattoshinigami@gmail.com>
2020-03-24 02:24:28 +0100

    Clean project

---
+++ lib/routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'package:hype_learning_flutter/screens/home/home.dart';
+
+final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+  "/": (BuildContext context) => Home(),
+  
+};
---
+++ lib/screens/home/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Card(
+          child: FloatingActionButton(onPressed: null),
+          
+      )),
+    );
+  }
+}
